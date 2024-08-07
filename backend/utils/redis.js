d191d267b065b60cbc38f66f074162d5e3c734f3
faheem59 <faheem@binmile.com>
2024-08-07 16:11:01 +0530

    depoly

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,37 +1,37 @@
-const redis = require('ioredis');
-const client = redis.createClient(6379);
+// const redis = require('ioredis');
+// const client = redis.createClient(6379);
 
-const expirationTime = 300;
-const movieKeyFormat = 'imdbID=';
+// const expirationTime = 300;
+// const movieKeyFormat = 'imdbID=';
 
-async function setCache(movieId, data) {
-    let key = movieKeyFormat + movieId;
-    return await set(key, JSON.stringify(data))
-}
-
-async function set(key, data) {
-    await client.setex(key, expirationTime, data);
-}
-
-async function getCache(movieId) {
-    let key = movieKeyFormat + movieId;
-    let data = await get(key);
-    return JSON.parse(data);
-}
-
-async function get(key) {
-    return await client.get(key);
-}
-
-// async function clearCache(movieId) {
+// async function setCache(movieId, data) {
 //     let key = movieKeyFormat + movieId;
-//     return await clear(key);
+//     return await set(key, JSON.stringify(data))
 // }
 
-// async function clear(key) {
-//     return await client.del(key);
+// async function set(key, data) {
+//     await client.setex(key, expirationTime, data);
 // }
 
-module.exports.getCache = getCache
-module.exports.setCache = setCache
-// module.exports.clearCache = clearCache
+// async function getCache(movieId) {
+//     let key = movieKeyFormat + movieId;
+//     let data = await get(key);
+//     return JSON.parse(data);
+// }
+
+// async function get(key) {
+//     return await client.get(key);
+// }
+
+// // async function clearCache(movieId) {
+// //     let key = movieKeyFormat + movieId;
+// //     return await clear(key);
+// // }
+
+// // async function clear(key) {
+// //     return await client.del(key);
+// // }
+
+// module.exports.getCache = getCache
+// module.exports.setCache = setCache
+// // module.exports.clearCache = clearCache
